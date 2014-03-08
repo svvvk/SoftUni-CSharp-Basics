89ed289b5561c39d6afb060317703350650ff999
MilushK <milush.karadimov@fdiba.tu-sofia.bg>
2014-03-08 19:12:57 +0200

    Update DebuggerTest.cs

--- IntroProgramming/10 PlayWithDebugger/DebuggerTest.cs
+++ IntroProgramming/10 PlayWithDebugger/DebuggerTest.cs
@@ -4,6 +4,8 @@ class DebuggerTest
 {
     static void Main(string[] args)
     {
+        Console.BufferHeight = 1001;
+        
         for (int i = 1; i <= 1000; i++)
         {
             Console.WriteLine(i);
