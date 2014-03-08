acb657de4cf164f48abf8dd829e475bed0230fac
MilushK <milush.karadimov@fdiba.tu-sofia.bg>
2014-03-08 18:55:29 +0200

    .init

--- IntroProgramming/08 AgeAfterTenYears/AgeAfterTenYears.cs
+++ IntroProgramming/08 AgeAfterTenYears/AgeAfterTenYears.cs
@@ -6,10 +6,32 @@ class AgeAfterTenYears
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter your current age: ");
-        byte currentAge = byte.Parse(Console.ReadLine());
-        byte newAge = (byte) (currentAge + 10);
-        Console.WriteLine("Age now: {0}", currentAge);
-        Console.WriteLine("Age after 10 years: {0}", newAge);
+        Console.WriteLine("Enter your birthyear: ");
+        int year = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Enter your birthmonth: ");
+        int month = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Enter your birthday: ");
+        int day = int.Parse(Console.ReadLine());
+
+        DateTime yourBirthday = new DateTime(year, month, day);
+        DateTime today =  DateTime.Today;
+        
+        if (today.Month >= yourBirthday.Month && today.Day >= yourBirthday.Day)
+        {
+            byte age = (byte)(today.Year - yourBirthday.Year);
+            byte newAge = (byte)(age + 10);
+            Console.WriteLine("Age now: {0}", age);
+            Console.WriteLine("Age after 10 years: {0}", newAge);
+        }
+        
+        else
+        {
+            byte age = (byte)(today.Year - yourBirthday.Year - 1);
+            byte newAge = (byte)(age + 10);
+            Console.WriteLine("Age now: {0}", age);
+            Console.WriteLine("Age after 10 years: {0}", newAge);
+        }
     }
 }
