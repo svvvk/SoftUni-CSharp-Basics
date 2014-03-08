13422d2bcca48435bf9105e28111d81ba3bd45c3
MilushK <milush.karadimov@fdiba.tu-sofia.bg>
2014-03-08 18:22:26 +0200

    .init

--- IntroProgramming/08 AgeAfterTenYears/AgeAfterTenYears.cs
+++ IntroProgramming/08 AgeAfterTenYears/AgeAfterTenYears.cs
@@ -1,11 +1,13 @@
-﻿//Print your age after ten years
+﻿//Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.
+
 using System;
 
 class AgeAfterTenYears
 {
     static void Main(string[] args)
     {
-        byte currentAge = 19;
+        Console.Write("Enter your current age: ");
+        byte currentAge = byte.Parse(Console.ReadLine());
         byte newAge = (byte) (currentAge + 10);
         Console.WriteLine("Age now: {0}", currentAge);
         Console.WriteLine("Age after 10 years: {0}", newAge);
